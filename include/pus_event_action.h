1a3c2e660ade219aefc3246af2091305e50819c1
Jose Miguel Sanchez <jomsanchez@gmv.com>
2017-11-28 13:53:39 +0100

    st19 add/delete/enable/disable definitions

--- include/pus_event_action.h
+++ include/pus_event_action.h
@@ -28,61 +28,9 @@ extern const size_t pus_st19_EventActionDefinitionListMaximum;
 
 extern pusError_t pus_eventAction_configure();
 
-pusError_t pus_eventAction_addEventActionDefinition(pusSt05EventId_t eventID, pusPacket_t* tcAction)
-{
-	//reject if
-	// 	- same event/action
-	//  - maximum reached
-	//  - action fail
-	// then generate failed start of execution
-
-	//TODO The event-action subservice shall process any valid instruction that is contained
-	//within a request to add event-action definitions regardless of the presence of faulty instructions.
 
 
-	//accept if
-	//  -
 
-	int i;
-	for(i = 0; i < pus_st19_EventActionDefinitionListMaximum; i++)
-	{
-		//check if exist
-
-		if( false == pus_st19_EventActionDefinitionList[i].deleted )
-		{
-			pus_st19_EventActionDefinitionList[i].eventID = eventID;
-			pus_st19_EventActionDefinitionList[i].definitionStatus = true;
-			pus_st19_EventActionDefinitionList[i].tcAction = *tcAction;
-			pus_st19_EventActionDefinitionList[i].deleted = false;
-
-		}
-	}
-
-	if(i == pus_st19_EventActionDefinitionListMaximum)
-	{
-		return PUS_ERROR_FULL_EVENT_ACTION_LIST;
-	}
-
-	return PUS_NO_ERROR;
-}
-
-pusError_t pus_eventAction_deleteEventActionDefinition()
-{
-
-	return PUS_NO_ERROR;
-}
-
-pusError_t pus_eventAction_enableEventActionDefinition()
-{
-
-	return PUS_NO_ERROR;
-}
-
-pusError_t pus_eventAction_disableEventActionDefinition()
-{
-
-	return PUS_NO_ERROR;
-}
 
 #ifdef __cplusplus
 }
