d095d37f2f61a725bc2892f18e29063e18ba22ed
Miguel Munoz <mmunoz@gmv.com>
2019-06-26 16:47:20 +0200

    Removed TASTE models for the ESROCOS orbital use case

--- taste.ur5_rtems/fast_skeletons.sh
+++
@@ -1,5 +0,0 @@
-# H2020 ESROCOS Project
-# Company: GMV Aerospace & Defence S.A.U.
-# Licence: GPLv2
-/home/esrocos/jmsm/taste-aadl-parser/aadl-parser/aadl_parser --gw -o output --glue -i ./InterfaceView.aadl -c ./DeploymentView.aadl -d ./DataView.aadl /home/esrocos/tool-inst/share/ocarina/AADLv2/ocarina_components.aadl
-cd output; make
--- taste.ur5_rtems/user_init_last.sh
+++
@@ -1,2 +0,0 @@
-# Renaming m_free symbol (conflict with RTEMS)
-sparc-rtems5-objcopy /home/esrocos/esrocos_workspace/install/gr740/lib/libesrocos_mpy_core_test_ur5_rtems.a --redefine-sym m_free_mp=m_free
