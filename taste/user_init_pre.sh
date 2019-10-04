548cdda2c123daf8132c4f482d4048d6535d6ee7
Raquel Jalvo <rjalvo@gmv.com>
2019-10-04 09:23:14 +0200

    chore: Updating python dependencies from python 3.5 to 3.6 for ubuntu 18.04 and including taste models in the build

--- taste/user_init_pre.sh
+++ taste/user_init_pre.sh
@@ -12,7 +12,7 @@ ORCHESTRATOR_OPTIONS+=" -l x86_partition:$AUTOPROJ_CURRENT_ROOT/install/lib/libe
 ORCHESTRATOR_OPTIONS+=" -l x86_partition:$AUTOPROJ_CURRENT_ROOT/install/lib/libesrocos_pus_core_test_01.so"
 
 # Native Python needed for interface with the PUS Console GUI
-ORCHESTRATOR_OPTIONS+=" -e x86_partition:/usr/include/python3.5m"
-ORCHESTRATOR_OPTIONS+=" -l x86_partition:/usr/lib/x86_64-linux-gnu/libpython3.5m.so"
+ORCHESTRATOR_OPTIONS+=" -e x86_partition:/usr/include/python3.6m"
+ORCHESTRATOR_OPTIONS+=" -l x86_partition:/usr/lib/x86_64-linux-gnu/libpython3.6m.so"
 
 echo "ORCHESTRATOR_OPTIONS=$ORCHESTRATOR_OPTIONS" 
