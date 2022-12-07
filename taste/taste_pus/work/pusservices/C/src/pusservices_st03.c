0b31815d2306d3d644e3c723a1959302fe7a6bb3
Carolina Serra <cpserra@gmv.com>
2022-12-07 13:42:51 +0000

    [ST03] Fix in TASTE model to allow disabling/enabling housekeeping

--- taste/taste_pus/work/pusservices/C/src/pusservices_st03.c
+++ taste/taste_pus/work/pusservices/C/src/pusservices_st03.c
@@ -23,12 +23,12 @@ void pusservices_initService03(void)
 {
     /*Initialization of Housekeeping*/
     pus_hk_initialize(NULL);
-	//pus_hk_setHK_PARAM_INT01(5);
-	//pus_hk_setHK_PARAM_REAL01(100.0);
-	//pus_hk_setHK_PARAM_INT02(6);
-	//pus_hk_setHK_PARAM_UINT01(10);
-	//pus_hk_setHK_PARAM_BYTE01(0x3);
-	//pus_hk_setHK_PARAM_BOOL01(false);
+	pus_hk_setHK_PARAM_INT01(5);
+	pus_hk_setHK_PARAM_REAL01(100.0);
+	pus_hk_setHK_PARAM_INT02(6);
+	pus_hk_setHK_PARAM_UINT01(10);
+	pus_hk_setHK_PARAM_BYTE01(0x3);
+	pus_hk_setHK_PARAM_BOOL01(false);
 }
 
 void pusservices_st03HkReportTrigger()
@@ -39,23 +39,22 @@ void pusservices_st03HkReportTrigger()
 	pusSequenceCount_t seqCount;
 	pusApid_t destination = 1;
 
-	pusSt03HousekeepingReportId_t reportId = pus_ST03_DEFAULT_HK_REPORT;
+	pusSt03HousekeepingReportId_t reportId;// = pus_ST03_DEFAULT_HK_REPORT;
 	pusSt03ReportInfo_t reportInfo;
-	if ( pus_ST03_DEFAULT_HK_REPORT == reportId )
-	{
+//	if ( pus_ST03_DEFAULT_HK_REPORT == reportId )
+//	{
 		//pus_hk_getReportParams
+
+	for(reportId = 0; reportId < PUS_HK_NUMBER_REPORTS; reportId++)
+	{
+		if(TRUE == pus_hk_isReportEnabled(reportId))
+		{
 		reportInfo.numParams = pus_st03_HkReportInfos[reportId].numParams;
 		reportInfo.paramIds = pus_st03_HkReportInfos[reportId].paramIds;
-	}
-	else
-	{
-		return;
-	}
 
     apid = pusservices_getApid();//st03_RI_getApid(&apid); 
     seqCount = pusservices_getTmSequenceCount();//st03_RI_getSequenceCount(&seqCount);
 	
-
 	pus_tm_3_25_createHousekeepingReport(&tm, apid, seqCount, reportId, destination);
 	pus_tm_3_25_setNumParameters(&tm, reportInfo.numParams);
 	pusStoredParam_t paramValue;
@@ -63,19 +62,21 @@ void pusservices_st03HkReportTrigger()
 	for( size_t i = 0; i < reportInfo.numParams; i++)
 	{
         isAvailable = pus_hk_getStoredParam(reportInfo.paramIds[i], &paramValue);//st03_RI_getParamValue(&reportInfo.paramIds[i], &paramValue, &isAvailable); 
-		printf("i: %lu, id: %llu, param:%llu\n", i, reportInfo.paramIds[i], paramValue);
+                printf("i: %lu, id: %llu, param:%llu\n", i, reportInfo.paramIds[i], paramValue);
 		if( PUS_NO_ERROR == isAvailable )
 		{
 			pus_tm_3_25_setParameterValue(&tm, i, paramValue);
 		}
 		else
 		{
-			printf("param not available\n");
+                    printf("param not available\n");
 		}
 	}
 
 
 	pusservices_PI_addTm(&tm); 
+		}
+	}
 }
 
 void pusservices_processTc03(const pusPacket_t *tcPacket)
